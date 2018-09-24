19854706e618cb868ad9d2cdfa9e6cc81de241ef
Ryan <hayes.ryan95@gmail.com>
2018-09-24 13:37:51 -0500

    User-signup is finished.

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-    <style>
-        .error {{ color: red; }}
-    </style>
-    <h1>Signup</h1>
-    <form method='POST'>
-        <label>Username
-            <input name="username" type="text" value='{username}'/>
-        </label>
-        <p class="error">{username_error}</p>
-
-        <label>Password
-            <input name="password" type="password" value='{password}'/>
-        </label>
-        <p class="error">{password_error}</p>
-
-        <label>Verify Password
-            <input name="verify_password" type="password" value='{verify_password}'/>
-        </label>
-        <p class="error">{verify_password_error}</p>
-
-
-        <input type="submit" />
-    </form>
-    """
 
 @app.route('/')
 def display_form():
-    return form.format(username='', username_error='', password='', password_error='', verify_password='', verify_password_error='')
+    template = jinja_env.get_template('form.html')
+    return template.render()
 
 def validate_username(user):
     if user == "":
@@ -56,7 +39,11 @@ def validate_password(pword):
     else:
         return True
 
-def verify_password(pword, verify_pword):
+def check_password(verify_pword, pword):
+    
+    if verify_pword == "":
+        return False
+    
     if pword != verify_pword:
         return False
     else:
@@ -73,24 +60,43 @@ def validate():
     password_error = ''
     verify_password_error = ''
 
-    if not validate_username(username):
-        username_error = "Not a valid username"
-        username = ''
+    if not check_password(verify_password, password):
+        verify_password_error = "Password and password-confirmation do not match"
 
     if not validate_password(password):
         password_error = "Not a valid password"
 
-    if not verify_password(password, verify_password):
-        verify_password_error = "Password and password-confirmation do not match"
+    if not validate_username(username):
+        username_error = "Not a valid username"
+        
+
+    if verify_password_error:
+        password = ''
+        verify_password = ''
+
+    if password_error:
+        password = ''
+        verify_password = ''
+
+    if username_error:
+        username = ''
         password = ''
         verify_password = ''
 
 
     if not username_error and not password_error and not verify_password_error:
-        return "Success, ya dink!"
+        user = str(username)
+        return redirect('/valid-login?user={0}'.format(user))
     else:
-       return form.format(username_error=username_error, password_error=password_error, verify_password_error=verify_password_error,
+       template = jinja_env.get_template('form.html')
+       return template.render(username_error=username_error, password_error=password_error, verify_password_error=verify_password_error,
        username=username, password=password, verify_password=verify_password)
 
+
+@app.route('/valid-login')
+def valid_login():
+    user = request.args.get('user')
+    welcome = jinja_env.get_template('welcome.html')
+    return welcome.render(name=user)
 
 app.run()
