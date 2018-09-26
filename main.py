c3ff61eacfcd70a8595fec6b0c1fe1e3d6770e71
Ryan <hayes.ryan95@gmail.com>
2018-09-26 13:20:58 -0500

    Final commit of user-signup, added email functionality, ready to turn in

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ def validate_password(pword):
         return True
 
 def check_password(verify_pword, pword):
-    
     if verify_pword == "":
         return False
     
@@ -49,16 +48,36 @@ def check_password(verify_pword, pword):
     else:
         return True
 
+def validate_email(email):
+    if email != "":
+        if email.count('@') != 1:
+            return False
+        if email.count('.') != 1:
+            return False
+        if " " in email:
+            return False
+        if len(email) > 20:
+            return False
+        if len(email) < 3:
+            return False
+        else:
+            return True
+    else:
+        return True
+
+
 @app.route('/', methods=['POST'])
 def validate():
 
     username = request.form['username']
     password = request.form['password']
     verify_password = request.form['verify_password']
+    email = request.form['email']
 
     username_error = ''
     password_error = ''
     verify_password_error = ''
+    email_error = ''
 
     if not check_password(verify_password, password):
         verify_password_error = "Password and password-confirmation do not match"
@@ -68,6 +87,9 @@ def validate():
 
     if not validate_username(username):
         username_error = "Not a valid username"
+
+    if not validate_email(email):
+        email_error = "Not a valid email"
         
 
     if verify_password_error:
@@ -83,14 +105,19 @@ def validate():
         password = ''
         verify_password = ''
 
+    if email_error:
+        email = ''
+        password = ''
+        verify_password = ''
 
-    if not username_error and not password_error and not verify_password_error:
+
+    if not username_error and not password_error and not verify_password_error and not email_error:
         user = str(username)
         return redirect('/valid-login?user={0}'.format(user))
     else:
        template = jinja_env.get_template('form.html')
        return template.render(username_error=username_error, password_error=password_error, verify_password_error=verify_password_error,
-       username=username, password=password, verify_password=verify_password)
+       email_error=email_error, username=username, password=password, verify_password=verify_password, email=email)
 
 
 @app.route('/valid-login')
